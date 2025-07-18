d3ba32e88e7937f7ab1dd64bc9e00e4dcf04b6ac
espindolas95 <espindolasol.ok@hotmail.com>
2025-07-18 19:46:36 +0200

    test(correzione di errori sintattici)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //recupero l'elemento UL  e il bottone
-const emaiList = document.getElementById ('email-list');
-const loadMails = document.getElementById ('load-mails');
+const emailList = document.getElementById ('email-list');
+const loadEmails = document.getElementById ('load-emails');
 
 
 //funzione che fa il fetch 10 volte per ottenere le diverse mail
 function fetchMails(){
     //svuoto la lista prima di aggiungere quelle nuove
-    emaiList.innerHTML='';
+    emailList.innerHTML='';
 
 
     for (let i=0; i<10; i++){
@@ -19,12 +19,14 @@ function fetchMails(){
         const li = document.createElement('li');
 
         //aggiungo la classe di bootstrap per avere uno stile piu ordinato e carino
-        la.classList.add('list-group-item');
+         li.classList.add('list-group-item');
         //metto l' email  come testo dentro il <li>
         li.textContent=email;
         //metto  l' <li> alla lista <ul>
-        emaiList.appendChild(li);
+        emailList.appendChild(li);
        } );  
     }
 
-}
+} 
+  // quando l'utente clicca il bottone,esseguiamo la funzione fetchMails
+  loadEmails.addEventListener('click', fetchMails);
