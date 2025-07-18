85f570d48f89bcf497831a5e9d14bb41e0f97032
espindolas95 <espindolasol.ok@hotmail.com>
2025-07-18 17:33:13 +0200

    feat(recupero elemento UL e button)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp =>{
+//recupero l'elemento UL  e il bottone
+const emaiList= document.getElementById ('email-list');
+const loadMails= document.getElementById ('load-mails');
 
-} )
+
+
+
+
+//axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp =>{
+
+//} )
