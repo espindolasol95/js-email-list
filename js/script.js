139866a122be66ef43fb0ccd335590485622c484
espindolas95 <espindolasol.ok@hotmail.com>
2025-07-18 17:37:03 +0200

    feat(function che fa il fetch )

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ const emaiList= document.getElementById ('email-list');
 const loadMails= document.getElementById ('load-mails');
 
 
-
-
+//funzione che fa il fetch 10 volte per ottenere le diverse mail
+function fetchMails(){
+    //svuoto la lista prima di aggiungere quelle nuove
+    emaiList.innerHTML='';
+    
+}
 
 //axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp =>{
 
